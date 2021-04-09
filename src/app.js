1339676a08664db50bdbf8c3d4dc5d30f0ca6562
adelariestra <48964880+adelariestra@users.noreply.github.com>
2021-04-09 11:04:39 -0300

    root route setup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 import express from 'express'
 import morgan from 'morgan'
 
+import pkg from '../package.json'
+
 const app = express();
 
-//config
+// Config
+app.set("pkg", pkg);
+app.set("port", process.env.PORT || 4000);
+
+// Set Up
 app.use(morgan('dev'));
 
 // routes
 app.get('/', (req,res)=>{
-    res.json('root');
+    res.json({
+        name: app.get("pkg").name,
+        description: app.get("pkg").description,
+        version: app.get("pkg").version,
+        author: app.get("pkg").author
+    });
 })
 
 export default app;
