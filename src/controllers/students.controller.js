c12fafa5074a1489d9ad5c115c4d42108bab205f
adelariestra <48964880+adelariestra@users.noreply.github.com>
2021-04-09 11:48:55 -0300

    cleaned basic db interaction

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -1,27 +1,17 @@
 import Student from '../models/Student'
 
-export const get = (req, res) => {
-    res.json([{
-        name: "manolo",
-        lname: "sanchez"
-    },
-    {
-        name: "manolo",
-        lname: "manoles"
-    }])
+export const get = async (req, res) => {
+    const students = await Student.find();
+
+    res.status(200).json(students)
 }
 
-export const create = (req, res) => {
-    console.log(req.body)
-
+export const create = async (req, res) => {
     const { fname, lname, dni, address } = req.body;
 
-    const newStudent = new Student({
-        fname: fname,
-        lname: lname,
-        dni: dni,
-        address: address
-    })
+    const newStudent = new Student({ fname, lname, dni, address })
 
-    res.json(newStudent);
+    await newStudent.save();
+
+    res.status(201).json(newStudent);
 }
