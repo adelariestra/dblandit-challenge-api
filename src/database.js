6ecbdb74c8a1fbfdff414c8b81b8503120795d71
adelariestra <48964880+adelariestra@users.noreply.github.com>
2021-04-10 19:46:28 -0300

    added test db configuration

--- src/database.js
+++ src/database.js
@@ -1,11 +1,16 @@
 import mongoose from 'mongoose'
+import config from './config/config.json'
 
-mongoose.connect("mongodb://localhost/dblanditdb", {
+console.log(process.env.NODE_ENV);
+
+const db_uri = process.env.NODE_ENV.trim() === 'dev' ? config.DB_DEV_URI : config.DB_TEST_URI; 
+
+mongoose.connect(db_uri, {
     // to avoid deprecated warnings
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
-    .then(db => console.log(`DB is connected`))
+    .then(db => console.log(`DB is connected: ${db_uri}`))
     .catch((err) => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from './app'
-import db from './database'
+import './database'
 
 app.listen(4000);
 
