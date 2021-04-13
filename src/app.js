492c3c1719c0e4d45da765bf01098b171940df0e
adelariestra <48964880+adelariestra@users.noreply.github.com>
2021-04-13 15:21:16 -0300

    allow origin from config

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 import pkg from '../package.json'
+import {ORIGIN_ALLOWED} from './config/config.json'
 
 import studentRoutes from './routes/student.routes'
 import courseRoutes from './routes/course.routes'
@@ -13,6 +15,7 @@ app.set("pkg", pkg);
 app.set("port", process.env.PORT || 4000);
 
 // Middlewares
+app.use(cors({origin:ORIGIN_ALLOWED}))
 app.use(express.json()); // detect json bodies
 app.use(morgan('dev')); // to show req on console
 
