e256ea10ed797eff3f1fca9cfdb55f325feacca9
adelariestra <48964880+adelariestra@users.noreply.github.com>
2021-04-16 15:20:21 -0300

    added bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = new Schema(
     {
@@ -18,5 +19,14 @@ const userSchema = new Schema(
         versionKey: false
     }
 );
+
+userSchema.statics.encryptPassword = async (psw) => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(psw, salt);
+};
+
+userSchema.statics.comparePassword = async (psw, pswToCompare) => {
+    return await bcrypt.compare(psw, pswToCompare)
+}
 
 export default model('User', userSchema);
