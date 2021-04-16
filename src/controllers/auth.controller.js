b21cded89ff7a6c5b98862e6a3c1ab634e6dc81d
adelariestra <48964880+adelariestra@users.noreply.github.com>
2021-04-16 16:29:24 -0300

    added login controller function

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,20 @@
 import User from '../models/User'
-
+import { PSW_SEED, SESSION_DURATION } from '../config/config.json'
 
 export const login = async (req, res) => {
-    console.log(req.body);
-    // query 
+    try {
+        const { username, password } = req.body;
+        const user = User.findOne({ username: username });
 
-    // res.status(200).json(...)
+        if (await User.comparePassword(password, user.password))
+            return res.status(401).json({ token: null, message: "Invalid password" });
+    
+        const token = jwt.sign({ id: user._id }, PSW_SEED, {
+            expiresIn: SESSION_DURATION
+        });
+
+        res.status(200).json({token:token});
+    } catch (e) { handleError(e, res); }
 }
 
 export const logout = async (req, res) => {
